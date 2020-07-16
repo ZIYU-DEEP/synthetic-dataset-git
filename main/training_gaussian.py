550535de509a41fc0981669e52ef97bad4da0552
Leksai Ye <ziyuye@uchicago.edu>
2020-07-15 19:08:44 -0500

    mu

--- main/training_gaussian.py
+++ main/training_gaussian.py
@@ -283,7 +283,7 @@ for mean in mean_d1i_list:
     # Get predicted label
     y_90 = [1 if e > cut_90 else 0 for e in scores]
     recall_90 = sum(y_90) / len(y_90)
-    d1i = ((np.array(mean) - abnormal_mu_) ** 2).sum()
+    d1i = ((np.array(mean) - [int(i) for i in abnormal_mu_.split('_')]) ** 2).sum()
 
     # Write results
     f.write('[mean={}; d0i=2.5, d1i={}; recall={}]\n'.format(mean, d1i, recall_90))
@@ -313,7 +313,7 @@ for mean in mean_d0i_list:
     # Get predicted label
     y_90 = [1 if e > cut_90 else 0 for e in scores]
     recall_90 = sum(y_90) / len(y_90)
-    d0i = ((np.array(mean) - normal_mu) ** 2).sum()
+    d0i = ((np.array(mean) - [int(i) for i in normal_mu.split('_')]) ** 2).sum()
 
     # Write results
     f.write('[mean={}; d0i={}; d1i=2.5; recall={}]\n'.format(mean, d0i, recall_90))
