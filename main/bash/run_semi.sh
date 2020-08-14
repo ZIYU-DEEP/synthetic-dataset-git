8ffb329183fc7faf807993df8c3eb3f720b93f04
Leksai Ye <ziyuye@uchicago.edu>
2020-08-14 21:58:57 +0800

    real

--- main/bash/run_semi.sh
+++ main/bash/run_semi.sh
@@ -1,3 +1,5 @@
+cd ..
+
 python training_gaussian_updated.py -nt gaussian3d_rec -op rec --step 30 -am_ 1_1_2 -am_add 1_1_2 -am 1_1_2
 python training_gaussian_updated.py -nt gaussian3d_one_class -op one_class --step 30 -am_ 1_1_2 -am_add 1_1_2 -am 1_1_2
 
--- main/bash/run_un.sh
+++ main/bash/run_un.sh
@@ -1,3 +1,4 @@
+cd ..
 python training_gaussian_updated.py -nt gaussian3d_rec -op rec_unsupervised --step 30 -am_ 1_1_2 -am_add 1_1_2
 python training_gaussian_updated.py -nt gaussian3d_one_class -op one_class_unsupervised --step 30 -am_ 1_1_2 -am_add 1_1_2
 
